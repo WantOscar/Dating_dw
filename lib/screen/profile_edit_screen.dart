956f6275e8477614a4c3fa947db18d1c165308ff
KimDongWook <kimdw3824@gmail.com>
2023-10-17 17:10:33 +0900

    프로필 수정 창 텍스트 필드 만지는 중

--- lib/screen/profile_edit_screen.dart
+++ lib/screen/profile_edit_screen.dart
@@ -24,35 +24,58 @@ class ProfileEditScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.start,
-            spacing: 1,
-            runSpacing: 1,
-            children: [
-              const ContainerBasic(),
-              const ContainerBasic(),
-              const ContainerBasic(),
-              const ContainerBasic(),
-              const ContainerBasic(),
-              Stack(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(2.0),
+              child: Row(
+                children: [
+                  ContainerBasic(),
+                  ContainerBasic(),
+                  ContainerBasic(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Row(
                 children: [
                   const ContainerBasic(),
-                  Positioned(
-                    top: 90,
-                    right: 1,
-                    child: IconButton(
-                      icon: IconShape.iconPhotoCamera,
-                      onPressed: () {},
-                    ),
+                  const ContainerBasic(),
+                  Stack(
+                    children: [
+                      const ContainerBasic(),
+                      Positioned(
+                        top: 90,
+                        right: 1,
+                        child: IconButton(
+                          icon: IconShape.iconPhotoCamera,
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 30),
+            const Text('이름'),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 0.03,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey,
+              ),
+              child: const TextField(
+                decoration: InputDecoration(hintText: '이름 입력'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
