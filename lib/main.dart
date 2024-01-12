49a9337eda46e088ef0a1e787c791d167c6cbfc7
KimDongWook <kimdw3824@gmail.com>
2024-01-12 15:16:30 +0900

    메인창 mbti별 추천 조합 카드 텍스트 overflow나는 것 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dating/binding/init_binding.dart';
 import 'package:dating/screen/auth/login_screen.dart';
+import 'package:dating/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      home: const LoginScreen(),
+      // home: const LoginScreen(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
       initialBinding: InitBinding(),
     );
