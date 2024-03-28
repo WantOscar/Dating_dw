eb2f43e952a727fa11e0d44dcc6723ea410815a5
KimDongWook <kimdw3824@gmail.com>
2024-03-28 20:08:41 +0900

    내 프로필 창에서 올린 사진들을(최대 6장) 슬라이드 하여 볼 수 있도록 ui/레이아웃 제작 완료

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dating/Widget/profile_edit/my_photos.dart';
 import 'package:dating/widget/common_header.dart';
-import 'package:dating/widget/profile_edit/my_photos.dart';
 import 'package:dating/widget/profile/personal_information.dart';
 import 'package:dating/widget/profile/personality.dart';
 import 'package:dating/widget/profile/interest.dart';
