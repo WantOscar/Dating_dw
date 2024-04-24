cb3ca79c02e0a8bb120eb110ccc4bf9a32a1f5dd
sangwook16074 <qazws78941@gmail.com>
2024-04-24 13:25:13 +0900

    토스트 메시지 UI 수정

--- lib/utils/toast_message.dart
+++ lib/utils/toast_message.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 
 mixin ToastMessage {
   static void showToast(String message) {
-    Fluttertoast.showToast(toastLength: Toast.LENGTH_SHORT, msg: message);
+    Fluttertoast.showToast(
+        backgroundColor: Colors.white,
+        textColor: Colors.black87,
+        toastLength: Toast.LENGTH_SHORT,
+        msg: message,
+        gravity: ToastGravity.CENTER);
   }
 }
