0b52d183dda3f43d4e42203e5210bf26f5ab649b
KimDongWook <kimdw3824@gmail.com>
2024-05-02 16:15:54 +0900

    안드로이드에서 발생하는 버전 삑 해결

--- lib/screen/profile/album_image.dart
+++ lib/screen/profile/album_image.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:photo_manager_image_provider/photo_manager_image_provider.dart';
 
 class AlbumImage extends StatelessWidget {
   final AssetEntity image;
