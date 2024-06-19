016308012461483b48c06e11b8ab02a043c6b119
sangwook16074 <qazws78941@gmail.com>
2024-06-19 14:37:31 +0900

    IOS플랫폼에 대한 FCM 연동 설정이 완료되었습니다. 기본적인 target이 13.0 이상으로 고정되었습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:dating/binding/login_binding.dart';
+import 'package:dating/firebase_options.dart';
 import 'package:dating/screen/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
