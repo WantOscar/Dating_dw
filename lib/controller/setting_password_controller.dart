9935a3674dd11545575217cf2c199245bd85bc12
KimDongWook <kimdw3824@gmail.com>
2024-06-19 16:22:15 +0900

    비밀번호 변경 창 controller 생성 후, 연결 완료

--- lib/controller/setting_password_controller.dart
+++ lib/controller/setting_password_controller.dart
@@ -1,8 +1,38 @@
 import 'package:dating/data/service/setting_password_service.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SettingPasswordController extends GetxController {
   final SettingPasswordService settingPasswordService;
-
   SettingPasswordController({required this.settingPasswordService});
+
+  static SettingPasswordController get to => Get.find();
+
+  final RxBool _isTextVisible = false.obs;
+  final RxString _validateNewPassword = "비밀번호가 일치하지 않습니다!".obs;
+  final RxString _newPw = "".obs;
+  final RxString _checkNewPw = "".obs;
+  final TextEditingController _currentPassword = TextEditingController();
+  final TextEditingController _newPassword = TextEditingController();
+  final TextEditingController _checkNewPassword = TextEditingController();
+
+  bool get isTextVisible => _isTextVisible.value;
+  String get validateNewPassword => _validateNewPassword.value;
+  String get newPw => _newPw.value;
+  String get checkNewPw => _checkNewPw.value;
+  TextEditingController get currentPassword => _currentPassword;
+  TextEditingController get newPassword => _newPassword;
+  TextEditingController get checkNewPassword => _checkNewPassword;
+
+  void toggleTextVisibility() {
+    _isTextVisible(!_isTextVisible.value);
+  }
+
+  void setNewPw(String value) {
+    _newPw(value);
+  }
+
+  void setCheckNewPw(String value) {
+    _checkNewPw(value);
+  }
 }
