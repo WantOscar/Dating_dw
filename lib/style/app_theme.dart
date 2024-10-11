cccf1171bea992ac7bd037119de3fe55b2e6b711
sangwook16074 <qazws78941@gmail.com>
2024-10-11 17:03:52 +0900

    탐색화면 백그라운드 컬러 변경

--- lib/style/app_theme.dart
+++ lib/style/app_theme.dart
@@ -9,17 +9,19 @@ ThemeData get lightTheme => ThemeData(
         primary: Color(0xFFFF006B),
         primaryContainer: Color(0xffffe6f0),
         secondary: Color(0xffffffff),
-        tertiary: Color(0xffffffff),
-        onSecondary: Color(0xff000000)));
+        tertiary: Color(0xffededed),
+        onSecondary: Color(0xff000000),
+        onTertiary: Color(0xffefefef)));
 
 ThemeData get darkTheme => ThemeData(
     useMaterial3: false,
     appBarTheme:
         const AppBarTheme(elevation: 0.0, backgroundColor: Color(0xff101010)),
-    scaffoldBackgroundColor: const Color(0xff101010),
+    scaffoldBackgroundColor: const Color(0xff000000),
     colorScheme: const ColorScheme.dark(
         primary: Color(0xFFFF006B),
         primaryContainer: Color(0xff444444),
         secondary: Color(0xff000000),
-        tertiary: Color(0xff464646),
-        onSecondary: Color(0xffffffff)));
+        tertiary: Color(0xff232323),
+        onSecondary: Color(0xffffffff),
+        onTertiary: Color(0xff101010)));
