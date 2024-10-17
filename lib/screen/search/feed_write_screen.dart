cf825b3dabf6de4f9e75e629531a5aa8b496c89a
WantOscar <kimdw3824@gmail.com>
2024-10-17 22:19:31 +0900

    글쓰기창 텍스트 사이즈 변경, 작성 완료 버튼 및에 검열 경고문 추가

--- lib/screen/search/feed_write_screen.dart
+++ lib/screen/search/feed_write_screen.dart
@@ -87,7 +87,7 @@ class _FeedWriteScreenState extends State<FeedWriteScreen> {
           color: ThemeColor.inputTextColor,
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 11.0, vertical: 17.0),
+          padding: const EdgeInsets.symmetric(horizontal: 11.0, vertical: 12),
           child: TextField(
             controller: _titleController,
             maxLength: 20,
@@ -116,14 +116,14 @@ class _FeedWriteScreenState extends State<FeedWriteScreen> {
     return Padding(
       padding: const EdgeInsets.only(left: 33.0, right: 33.0, bottom: 10),
       child: Container(
-        height: 300,
+        height: 298,
         width: double.infinity,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: ThemeColor.inputTextColor,
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 11.0, vertical: 17.0),
+          padding: const EdgeInsets.symmetric(horizontal: 11.0, vertical: 12.0),
           child: TextField(
             textAlignVertical: const TextAlignVertical(y: -1),
             controller: _contentController,
@@ -135,6 +135,7 @@ class _FeedWriteScreenState extends State<FeedWriteScreen> {
             decoration: const InputDecoration(
               hintText: '글을 작성해주세요.',
               hintStyle: TextStyle(fontSize: 15.0),
+              isDense: true,
               border: InputBorder.none,
               counterText: '',
             ),
@@ -148,33 +149,61 @@ class _FeedWriteScreenState extends State<FeedWriteScreen> {
   /// 처음 글 작성을 완료한 후,
   /// 이 버튼을 누르면 탐색 창에서 작성한 글이 올라감.
   Widget _completeButton() {
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 33.0, vertical: 35.0),
-        child: SafeArea(
-          bottom: true,
-          child: BottomButton(
-            onTap: () {
-              if (widget.feed != null) {
-                Feed updatedFeed = Feed(
-                    id: widget.feed!.id,
-                    title: _titleController.text,
-                    content: _contentController.text);
-                FeedController.to.updateDialog(updatedFeed);
-              } else {
-                FeedController.to.completeFeed();
-              }
-            },
-            child: const Text(
-              "작성 완료",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
+    return SafeArea(
+      bottom: true,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            color: Colors.white,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 33.0, vertical: 35.0),
+              child: SafeArea(
+                bottom: true,
+                child: BottomButton(
+                  onTap: () {
+                    if (widget.feed != null) {
+                      Feed updatedFeed = Feed(
+                          id: widget.feed!.id,
+                          title: _titleController.text,
+                          content: _contentController.text);
+                      FeedController.to.updateDialog(updatedFeed);
+                    } else {
+                      FeedController.to.completeFeed();
+                    }
+                  },
+                  child: const Text(
+                    "작성 완료",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            color: ThemeColor.warningText,
+            child: Center(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: Text(
+                  '개인정보 및 SNS 계정 공유, 커뮤니티에 맞지 않는 부적절한 콘텐츠의 경우 검열 및 제재 될 수 있음을 알려드립니다.',
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: ThemeColor.fontColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
